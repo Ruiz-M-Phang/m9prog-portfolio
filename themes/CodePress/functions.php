66ac4bd735d6c2dbda2e371d587c154b98a791c3
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2026-09-10 15:03:10 +0200

    wordpress stappen

---
+++ themes/CodePress/functions.php
@@ -0,0 +1,3 @@
+<?php
+
+# Plaats hierin de benodigde functies, laat het leeg als je nog geen nieuwe filters of functies hebt.
---
+++ themes/CodePress/index.php
@@ -0,0 +1,9 @@
+ <?php
+
+get_header();
+
+while ($a <= 10) {
+    # code...
+}
+
+get_footer();
